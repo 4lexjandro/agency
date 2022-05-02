f95349ea305710aa9cd1879774c8568b9a672250
Alexjandro Mendoza <alexjandro.mendoza21@gmail.com>
2022-05-01 23:25:14 -0400

    animation finally working after about 2 hours, thanks to codebucks, one of my many role models. It is an ugly animation but it is a start

--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -1,24 +1,55 @@
-import React from 'react'
+import { useSpring, animated } from 'react-spring'
+import styled, { keyframes } from "styled-components";
+import CM from "../assets/CM.jpeg"
+
+
+
+
+const move = keyframes`
+0% { transform: translateY(-5px)         }
+    50% { transform: translateY(10px) translateX(10px)        }
+    100% { transform: translateY(-5px)         }
+`;
+
+
+
+
 
 function Content() {
 
-
-
-
+  const Rocket = styled.div`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  width: 40%;
+  padding-bottom: 5rem;
+  animation: ${move} 2.5s ease infinite;
+  @media only Screen and (max-width: 40em) {
+    width: 50vw;
+    padding-bottom: 0;
+  }
+`;
+  
 
   
+
+
+
+
+
+
   return (
     <div>
-
-
-
-        <h1>My App</h1>
-
-
-
-
-    </div>
+     <h1>pow</h1>
+     <Rocket>
+            <img src={CM} alt="" width="400" height="400" />
+      </Rocket>
+       
+       
+      
+    
+      </div>
   )
 }
 
-export default Content
+export default Content;
